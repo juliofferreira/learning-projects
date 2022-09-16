84a04dc5aac751c6dbabb382060a17b2508fa31f
Julio Faria <jcesarffaria@gmail.com>
2022-09-16 14:57:36 -0300

    change formatting

--- html-css/web-accessibility/web-accessibility/js/carousel.js
+++ html-css/web-accessibility/web-accessibility/js/carousel.js
@@ -1,15 +1,13 @@
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
 
 // Percorre todos os botoes controladores
-btns.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    
+btns.forEach(function (btn) {
+	btn.addEventListener('click', function () {
+		// Remove classe 'ativo' dos outros botoes
+		btns.forEach(function (btnRemoveClass) {
+			btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo');
+		});
 
-    // Remove classe 'ativo' dos outros botoes
-    btns.forEach(function(btnRemoveClass) {
-      btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
-    })
-
-    this.classList.add('listaDeArtigos-slider-item--ativo')
-  })
-})
+		this.classList.add('listaDeArtigos-slider-item--ativo');
+	});
+});
--- html-css/web-accessibility/web-accessibility/js/dialog.js
+++ html-css/web-accessibility/web-accessibility/js/dialog.js
@@ -4,16 +4,17 @@ var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
-
 // Quando abrir a dialog...
-btnAbreDialog.addEventListener('click', function() {
-  dialog.classList.add('dialogNewsletter--aberto');
+btnAbreDialog.addEventListener('click', function () {
+	dialog.classList.add('dialogNewsletter--aberto');
 });
 
 function fechandoDialog() {
-  document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+	document.activeElement.blur();
+	dialog.classList.remove('dialogNewsletter--aberto');
 }
 
 // Listeners
-document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+document
+	.querySelector('.dialogNewsletter-fechar')
+	.addEventListener('click', fechandoDialog);
