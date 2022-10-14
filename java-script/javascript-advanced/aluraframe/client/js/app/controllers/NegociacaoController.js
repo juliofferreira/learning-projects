baeb13d7a1e529c422fa8b284a93ab6eb8beb5e1
Julio Faria <jcesarffaria@gmail.com>
2022-10-14 19:07:28 -0300

    progress js course

--- java-script/javascript-advanced/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ java-script/javascript-advanced/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,21 +5,29 @@ class NegociacaoController {
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
+		this._listaNegociacoes = new ListaNegociacoes();
 	}
 
 	adiciona(event) {
 		event.preventDefault();
 
-		let data = DateHelper.textoParaData(this._inputData.value);
+		this._listaNegociacoes.adiciona(this._criaNegociacao);
+		this._limpaFormulario();
+	}
 
-		let negociacao = new Negociacao(
-			data,
+	_criaNegociacao() {
+		return new Negociacao(
+			DateHelper.textoParaData(this._inputData.value),
 			this._inputQuantidade.value,
 			this._inputValor.value
 		);
+	}
 
-		let diaMesAno = DateHelper.dataParaTexto(negociacao.data);
+	_limpaFormulario() {
+		this._inputData.value = '';
+		this._inputQuantidade.value = 1;
+		this._inputValor.value = 0.0;
 
-		console.log(negociacao, diaMesAno);
+		this._inputData.focus();
 	}
 }
