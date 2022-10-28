9781e43f4d2d284e6f13013323a8bc6980fee2ea
Julio Faria <jcesarffaria@gmail.com>
2022-10-28 17:09:12 -0300

    start working with files course C#

---
+++ c-sharp/working-with-files/ByteBankIO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using ByteBankIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024];
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+        //public override int Read(byte[] array, int offset, int count);
+
+        Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+        
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+
+        //foreach (var meuByte in buffer)
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
+    }
+}
