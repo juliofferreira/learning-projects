d14ab11e02bfa2346ac7c1d27bfdf39b574756b1
Julio Faria <jcesarffaria@gmail.com>
2023-01-10 16:12:59 -0300

    finish webpack course

--- java-script/webpack/webpack-alura/webpack.config.js
+++ java-script/webpack/webpack-alura/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: './app/src/js/app.js',
@@ -9,13 +12,25 @@ module.exports = {
 		clean: true,
 	},
 	module: {
-		rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] }],
+		rules: [
+			{ test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerWebpackPlugin(), '...'],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './app/src/app.html',
-			filename: 'app.html',
+			template: './app/src/index.html',
+			filename: 'index.html',
 			hash: true,
 		}),
+		new MiniCssExtractPlugin({ filename: 'style.css' }),
+		new webpack.optimize.ModuleConcatenationPlugin(),
 	],
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		port: 3000,
+	},
 };
