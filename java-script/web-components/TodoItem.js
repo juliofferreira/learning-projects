189a2ed79e16d691a41311d7247aa8f609cf2b08
juliofferreira <jcesarffaria@gmail.com>
2023-02-18 18:03:04 -0300

    add web components file

---
+++ java-script/web-components/TodoItem.js
@@ -0,0 +1,56 @@
+const template = document.createElement('template');
+template.innerHTML = `
+	<style>
+		label {
+			color: red;
+			display: block;
+		}
+
+		.description {
+			font-size: .65rem;
+			font-weight: lighter;
+			color: #777;
+		}
+	</style>
+	<label>
+		<input type="checkbox" />
+		<slot></slot>
+		<span class="description">
+			<slot name="description"></slot>
+		</span>
+	</label>
+`;
+
+class TodoItem extends HTMLElement {
+	constructor() {
+		super();
+		const shadow = this.attachShadow({ mode: 'open' });
+		shadow.append(template.content.cloneNode(true));
+		this.checkbox = shadow.querySelector('input');
+	}
+
+	static get observedAttributes() {
+		return ['checked'];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name === 'checked') this.updateChecked(newValue);
+	}
+
+	connectedCallback() {
+		console.log('connected');
+	}
+
+	disconnectedCallback() {
+		console.log('disconnected');
+	}
+
+	updateChecked(value) {
+		this.checkbox.checked = value != null && value !== 'false';
+	}
+}
+
+customElements.define('todo-item', TodoItem);
+
+const item = document.querySelector('todo-item');
+item.remove();
